43641b2efe3cae08665d1b1eebe5898da19a2d74
ninofiliu <nino.filiu@telecom-paristech.fr>
2019-10-30 22:18:36 +0100

    add function promptTargetBranch

--- cli.js
+++ cli.js
@@ -1,22 +1,38 @@
 const {exec: execCb} = require('child_process');
 const {promisify} = require('util');
+const prompts = require('prompts');
 const exec = promisify(execCb);
 
 (async () => {
     const {branches, currenBranchIndex} = await getBranches();
-})();
+    const targetBranch = await promptTargetBranch();
 
-async function getBranches() {
-    const {stdout, stderr} = await exec('git branch', {encoding: 'utf8'});
-    if (stderr!='') {
-        throw new Error(`Error while running "git branch": ${stderr}`);
+    async function getBranches() {
+        const {stdout, stderr} = await exec('git branch', {encoding: 'utf8'});
+        if (stderr!='') {
+            throw new Error(`Error while running "git branch": ${stderr}`);
+        }
+        const branches = [];
+        let currentBranchIndex;
+        const branchLines = stdout.trim().split('\n');
+        branchLines.forEach((branchLine, index) => {
+            if (branchLine.startsWith('*')) currentBranchIndex = index;
+            branches.push(branchLine.substring(2));
+        });
+        return {branches, currentBranchIndex};
     }
-    const branches = [];
-    let currentBranchIndex;
-    const branchLines = stdout.trim().split('\n');
-    branchLines.forEach((branchLine, index) => {
-        if (branchLine.startsWith('*')) currentBranchIndex = index;
-        branches.push(branchLine.substring(2));
-    });
-    return {branches, currentBranchIndex};
-}
+
+    async function promptTargetBranch() {
+        const {branch} = await prompts({
+            type: 'select',
+            message: 'Select branch to checkout to',
+            name: 'branch',
+            choices: branches.map((branch) => ({
+                title: branch,
+                value: branch,
+            })),
+            initial: currenBranchIndex,
+        });
+        return branch;
+    }
+})();
