2720f778ca8c1753d36707b684d15c0b17ed770b
ninofiliu <nino.filiu@telecom-paristech.fr>
2019-10-30 21:11:04 +0100

    add function getBranches

--- cli.js
+++ cli.js
@@ -1,1 +1,22 @@
-console.log('for now, I do nothing.');
+const {exec: execCb} = require('child_process');
+const {promisify} = require('util');
+const exec = promisify(execCb);
+
+(async () => {
+    const {branches, currenBranchIndex} = await getBranches();
+})();
+
+async function getBranches() {
+    const {stdout, stderr} = await exec('git branch', {encoding: 'utf8'});
+    if (stderr!='') {
+        throw new Error(`Error while running "git branch": ${stderr}`);
+    }
+    const branches = [];
+    let currentBranchIndex;
+    const branchLines = stdout.trim().split('\n');
+    branchLines.forEach((branchLine, index) => {
+        if (branchLine.startsWith('*')) currentBranchIndex = index;
+        branches.push(branchLine.substring(2));
+    });
+    return {branches, currentBranchIndex};
+}
